86a78cf512a4c522c2bfe4b4790bd41c3f804da8
Bharat Dangat <139533258+bharatdangat@users.noreply.github.com>
2023-08-07 19:00:13 +0530

    Add files via upload

---
+++ Fun circle.py
@@ -0,0 +1,12 @@
+#WRITE FUNCTION TO CALCULATE AREA AND CIRCUMFERENCE OF CIRCLE IF RADIUS IS GIVEN.
+def circle(r):
+    a=3.14*r*r
+    c=2*3.14*r
+    print("Area Of Circle=",a)
+    print("Circumference Of Circle=",c)
+
+r=int(input("Enter Radius ="))
+circle(r)
+ 
+    
+    
