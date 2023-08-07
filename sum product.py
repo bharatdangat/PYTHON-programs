4ac4c026e2fa2456452a98d1ea426d92e6825a8c
Bharat Dangat <139533258+bharatdangat@users.noreply.github.com>
2023-08-07 19:01:37 +0530

    Add files via upload

---
+++ sum product.py
@@ -0,0 +1,15 @@
+#WRITE FUNCTION TO CALCULATE SUM AND PRODUCT OF THREE  NUMBERS.
+def sp(a,b,c):
+    sum=a+b+c
+    product=a*b*c
+    print("Sum Of Numbers=",sum)
+    print("Product Of Numbers=",product)
+
+a=int(input("Enter First Number"))
+b=int(input("Enter Second Number"))
+c=int(input("Enter Third Number"))
+sp(a,b,c)
+
+
+    
+
